927beffc7b8fe98635080af1b884183e16e2b31d
Adam Lalani <adam_lalani@brown.edu>
2023-07-12 17:56:50 -0400

    component selctor edits

--- components/ComponentSelector.js
+++ components/ComponentSelector.js
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 const KnowledgeGraph = dynamic(() => import('./kg'));
 const Enrichment = dynamic(()=>import("./Enrichment"));
 const Markdown = dynamic(()=>import("./markdown"), {ssr: false});
-const PPI = dynamic(()=>import('./ppiKG'));
+const PPI = dynamic(()=>import('./ppiKG/interactive'));
 
 export const components = {
   Enrichment: (props) => <Enrichment {...props}/>,
