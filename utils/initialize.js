e4aead775b3a6a75b283aa485dac50596d4d604c
Adam Lalani <adam_lalani@brown.edu>
2023-07-13 11:26:23 -0400

    revert edits initialize

--- utils/initialize.js
+++ utils/initialize.js
@@ -81,9 +81,6 @@ export const fetch_kg_schema = async () => {
 	return schema
   }
 
-export const initialize_ppi = async () => {
-	return 
-}
 
 export const initialize_kg = async () => {
 	const entries = {}
