0a4609536f0b62722b8d5020a8f5b96ea3287403
Adam Lalani <adam_lalani@brown.edu>
2023-07-13 14:27:20 -0400

    textbox processing file

--- components/ppiKG/interactive.js
+++ components/ppiKG/interactive.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TextBox from './textbox';
 
 const TextPage = () => {
   return (
     <div>
       <h1>Welcome to the Text Page</h1>
+      <TextBox />
       <p>This is a simple text page in React.</p>
     </div>
   );
