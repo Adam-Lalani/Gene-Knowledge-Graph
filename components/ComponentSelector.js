6bea14f4149b91c83212d488a265cda3184ca5ce
Adam Lalani <adam_lalani@brown.edu>
2023-07-24 10:14:20 -0400

    path change

--- components/ComponentSelector.js
+++ components/ComponentSelector.js
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 const KnowledgeGraph = dynamic(() => import('./kg'));
 const Enrichment = dynamic(()=>import("./Enrichment"));
 const Markdown = dynamic(()=>import("./markdown"), {ssr: false});
-const PPI = dynamic(()=>import('./ppiKG/interactive'));
+const PPI = dynamic(()=>import('./ppiKG'));
 
 export const components = {
   Enrichment: (props) => <Enrichment {...props}/>,
